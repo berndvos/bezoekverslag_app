13871553749973a45b300215de9aa4a2fa3154d4
Bernd Vos <berndvos@gmail.com>
2025-10-25 13:53:44 +0200

    Fix: Branding::get() must return an array

--- config/branding.php
+++ config/branding.php
@@ -6,6 +6,6 @@ final class Branding
     public static function get(): array
     {
         $file = __DIR__ . DIRECTORY_SEPARATOR . 'branding-settings.php';
-        return \file_exists($file) ? require_once $file : [];
+        return \file_exists($file) ? require $file : [];
     }
 }
